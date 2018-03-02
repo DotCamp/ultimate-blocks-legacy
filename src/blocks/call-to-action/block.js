a76cddf91bc0120dbdd8ad38c1b926a9b4ad0640
Imtiaz Rayhan <irayhan.asif@gmail.com>
2018-03-02 00:01:50 -0500

    Completed CTA block

--- src/blocks/call-to-action/block.js
+++ src/blocks/call-to-action/block.js
@@ -11,30 +11,22 @@ const { Component } = wp.element;
 const {
     registerBlockType,
     RichText,
-    AlignmentToolbar,
-    BlockControls,
-    BlockAlignmentToolbar,
-    MediaUpload,
     ColorPalette,
-    UrlInput,
     InspectorControls,
+    UrlInput,
 } = wp.blocks;
 
 const {
     Toolbar,
     Button,
-    IconButton,
-    Tooltip,
     PanelColor,
     PanelBody,
     PanelRow,
+    Dashicon,
+    IconButton,
     FormToggle,
     RangeControl
 } = wp.components;
-
-const {
-    SelectControl,
-} = wp.blocks.InspectorControls;
 
 /**
  * Register: aa Gutenberg Block.
@@ -56,7 +48,7 @@ registerBlockType( 'ub/call-to-action', {
     category: 'formatting', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
     keywords: [
         __( 'call to action' ),
-        __( 'Conversion' ),
+        __( 'conversion' ),
         __( 'optimize' ),
     ],
     attributes: {
@@ -64,13 +56,19 @@ registerBlockType( 'ub/call-to-action', {
             type: 'array',
             source: 'children',
             selector: '.ub_call_to_action_headline_text',
-            default: 'This is the Headline of the Call to Action Box'
+            default: 'This is the headline.'
         },
-        ub_call_to_action_content_text: {
+        ub_cta_content_text: {
             type: 'array',
             source: 'children',
-            selector: '.ub_call_to_action_content_text',
-            default: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed scelerisque sapien. Nulla fermentum in leo ut consectetur. In facilisis id tellus vitae vulputate. Sed tincidunt turpis eu turpis eleifend scelerisque. Cras posuere nisl iaculis augue ultricies, non volutpat velit tincidunt. Donec sed libero sit amet augue finibus ullamcorper nec at erat. In hac habitasse platea dictumst. '
+            selector: '.ub_cta_content_text',
+            default: 'Content area for the call to action box. Utilize it to state why the action should be taken.'
+        },
+        ub_cta_button_text: {
+            type: 'array',
+            source: 'children',
+            selector: '.ub_cta_button_text',
+            default: 'Get Started'
         },
         headFontSize: {
             type: 'number',
@@ -87,9 +85,43 @@ registerBlockType( 'ub/call-to-action', {
         contentColor: {
             type: 'string',
             default: '#444444'
-        }
-    },
+        },
+        buttonFontSize: {
+            type: 'number',
+            default: 14
+        },
+        buttonColor: {
+            type: 'string',
+            default: '#E27330'
+        },
+        buttonTextColor: {
+            type: 'string',
+            default: '#ffffff'
+        },
+        buttonWidth: {
+            type: 'number',
+            default: 250
+        },
+        ctaBackgroundColor: {
+            type: 'string',
+            default: '#f8f8f8'
+        },
+        ctaBorderColor: {
+            type: 'string',
+            default: '#ECECEC'
+        },
+        ctaBorderSize: {
+            type: 'number',
+            default: 2
+        },
+        url: {
+            type: 'string',
+            source: 'attribute',
+            selector: 'a',
+            attribute: 'href',
+        },
 
+    },
 
     /**
      * The edit function describes the structure of your block in the context of the editor.
@@ -101,18 +133,25 @@ registerBlockType( 'ub/call-to-action', {
      */
     edit: function( props ) {
 
-        const onChangeHeadlineText = value => {
-            props.setAttributes( { ub_call_to_action_headline_text: value } );
-        };
-
-        const onChangeContentText = value => {
-            props.setAttributes( { ub_call_to_action_content_text: value } );
-        };
-
+        // Creates a <p class='wp-block-cgb-block-click-to-tweet-block'></p>.
         return [
 
             !! props.focus && (
                 <InspectorControls key="inspectors">
+
+                    <p>Background Color</p>
+                    <ColorPalette
+                        value={ props.attributes.ctaBackgroundColor }
+                        onChange={ ( colorValue ) => props.setAttributes( { ctaBackgroundColor: colorValue } ) }
+                        allowReset
+                    />
+
+                    <p>Border Color</p>
+                    <ColorPalette
+                        value={ props.attributes.ctaBorderColor }
+                        onChange={ ( colorValue ) => props.setAttributes( { ctaBorderColor: colorValue } ) }
+                        allowReset
+                    />
 
                     <PanelBody
                         title={ __( 'Headline Settings' ) }
@@ -158,11 +197,60 @@ registerBlockType( 'ub/call-to-action', {
 
                     </PanelBody>
 
+                    <PanelBody
+                        title={ __( 'Button Settings' ) }
+                        initialOpen={ false }
+                    >
+
+                        <RangeControl
+                            label={ __( 'Button Width' ) }
+                            value={ props.attributes.buttonWidth }
+                            onChange={ ( value ) => props.setAttributes( { buttonWidth: value } ) }
+                            min={ 10 }
+                            max={ 500 }
+                            beforeIcon="editor-code"
+                            allowReset
+                        />
+
+                        <RangeControl
+                            label={ __( 'Font Size' ) }
+                            value={ props.attributes.buttonFontSize }
+                            onChange={ ( value ) => props.setAttributes( { buttonFontSize: value } ) }
+                            min={ 10 }
+                            max={ 200 }
+                            beforeIcon="editor-textcolor"
+                            allowReset
+                        />
+                        <p>Button Color</p>
+                        <ColorPalette
+                            value={ props.attributes.buttonColor }
+                            onChange={ ( colorValue ) => props.setAttributes( { buttonColor: colorValue } ) }
+                        />
+
+                        <p>Button Text Color</p>
+                        <ColorPalette
+                            value={ props.attributes.buttonTextColor }
+                            onChange={ ( colorValue ) => props.setAttributes( { buttonTextColor: colorValue } ) }
+                        />
+
+                        <br/>
+
+                    </PanelBody>
+                    <br/>
                 </InspectorControls>
+
             ),
 
             <div className={ props.className }>
-                <div className="ub_call_to_action">
+                <div
+                    className="ub_call_to_action"
+                    style={{
+                        backgroundColor: props.attributes.ctaBackgroundColor,
+                        border: props.attributes.ctaBorderSize + 'px solid',
+                        borderColor: props.attributes.ctaBorderColor
+                    }}
+                >
+
                     <div className="ub_call_to_action_headline">
                         <RichText
                             tagName="p"
@@ -171,29 +259,71 @@ registerBlockType( 'ub/call-to-action', {
                                 fontSize: props.attributes.headFontSize + 'px',
                                 color: props.attributes.headColor
                             }}
-                            onChange={ onChangeHeadlineText }
+                            onChange={ ( value ) => props.setAttributes( { ub_call_to_action_headline_text: value } ) }
                             value={ props.attributes.ub_call_to_action_headline_text }
                             focus={ props.focus }
                             keepPlaceholderOnFocus={true}
                         />
                     </div>
+
                     <div className="ub_call_to_action_content">
                         <RichText
                             tagName="p"
-                            className="ub_call_to_action_content_text"
+                            className="ub_cta_content_text"
                             style={{
                                 fontSize: props.attributes.contentFontSize + 'px',
                                 color: props.attributes.contentColor
                             }}
-                            onChange={ onChangeContentText }
-                            value={ props.attributes.ub_call_to_action_content_text }
+                            onChange={ ( value ) => props.setAttributes( { ub_cta_content_text: value } ) }
+                            value={ props.attributes.ub_cta_content_text }
                             focus={ props.focus }
                             keepPlaceholderOnFocus={true}
                         />
                     </div>
+
                     <div className="ub_call_to_action_button">
-                        <a href="#">Let's Get Started!</a>
+                        <span
+                            className={`wp-block-button ub_cta_button`}
+                            style={{
+                                backgroundColor: props.attributes.buttonColor,
+                                width: props.attributes.buttonWidth + 'px'
+                            }}
+                        >
+                            <RichText
+                                tagName="p"
+                                className="ub_cta_button_text"
+                                style={{
+                                    color: props.attributes.buttonTextColor,
+                                    fontSize: props.attributes.buttonFontSize + 'px'
+                                }}
+                                onChange={ ( value ) => props.setAttributes( { ub_cta_button_text: value } ) }
+                                value={ props.attributes.ub_cta_button_text }
+                                focus={ props.focus }
+                                keepPlaceholderOnFocus={true}
+                            />
+                        </span>
                     </div>
+                </div>
+                <div className="ub_call_to_action_url_input">
+                    {
+                        focus && (
+                            <form
+                                key={ 'form-link' }
+                                onSubmit={ ( event ) => event.preventDefault() }
+                                className={ `blocks-button__inline-link ub_cta_url_input_box`}>
+                                <Dashicon icon={ 'admin-links' } />
+                                <UrlInput
+                                    value={ props.attributes.url }
+                                    onChange={ ( value ) => props.setAttributes( { url: value } ) }
+                                />
+                                <IconButton
+                                    icon={ 'editor-break' }
+                                    label={ __( 'Apply' ) }
+                                    type={ 'submit' }
+                                />
+                            </form>
+                        )
+                    }
                 </div>
             </div>
         ];
@@ -210,15 +340,56 @@ registerBlockType( 'ub/call-to-action', {
     save: function( props ) {
         return (
             <div className={ props.className }>
-                <div className="ub_call_to_action">
+                <div
+                    className="ub_call_to_action"
+                    style={{
+                        backgroundColor: props.attributes.ctaBackgroundColor,
+                        border: props.attributes.ctaBorderSize + 'px solid',
+                        borderColor: props.attributes.ctaBorderColor
+                    }}
+                >
                     <div className="ub_call_to_action_headline">
-                        <p className="ub_call_to_action_headline_text">{ props.attributes.ub_call_to_action_headline_text }</p>
+                        <p
+                            className="ub_call_to_action_headline_text"
+                            style={{
+                                fontSize: props.attributes.headFontSize + 'px',
+                                color: props.attributes.headColor
+                            }}
+                        >
+                            { props.attributes.ub_call_to_action_headline_text }
+                        </p>
                     </div>
                     <div className="ub_call_to_action_content">
-                        <p className="ub_call_to_action_content_text">{ props.attributes.ub_call_to_action_content_text }</p>
+                        <p
+                            className="ub_cta_content_text"
+                            style={{
+                                fontSize: props.attributes.contentFontSize + 'px',
+                                color: props.attributes.contentColor
+                            }}
+                        >
+                            { props.attributes.ub_cta_content_text }
+                        </p>
                     </div>
                     <div className="ub_call_to_action_button">
-                        <a href="#">Let's Get Started!</a>
+                        <span
+                            className={`wp-block-button ub_cta_button`}
+                            style={{
+                                backgroundColor: props.attributes.buttonColor,
+                                width: props.attributes.buttonWidth + 'px'
+                            }}
+                        >
+                            <a href={ props.attributes.url } target="_blank">
+                                <p
+                                    className="ub_cta_button_text"
+                                    style={{
+                                        color: props.attributes.buttonTextColor,
+                                        fontSize: props.attributes.buttonFontSize + 'px'
+                                    }}
+                                >
+                                    { props.attributes.ub_cta_button_text }
+                                </p>
+                            </a>
+                        </span>
                     </div>
                 </div>
             </div>
