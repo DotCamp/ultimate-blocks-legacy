dffa38547036d67310d6d8014ae83e2c548ee5c9
Imtiaz Rayhan <irayhan.asif@gmail.com>
2018-03-14 02:17:57 -0400

    Feature box text functional

--- src/blocks/feature-box/block.js
+++ src/blocks/feature-box/block.js
@@ -53,6 +53,48 @@ registerBlockType( 'ub/feature-box', {
 		__( 'Column' ),
 		__( 'Feature Image' ),
 	],
+    attributes: {
+        column: {
+            type: 'select',
+            default: '2'
+        },
+        columnOneTitle: {
+            type: 'array',
+            source: 'children',
+            selector: '.ub_feature_one_title',
+            default: 'Title One'
+        },
+        columnTwoTitle: {
+            type: 'array',
+            source: 'children',
+            selector: '.ub_feature_two_title',
+            default: 'Title Two'
+        },
+        columnThreeTitle: {
+            type: 'array',
+            source: 'children',
+            selector: '.ub_feature_three_title',
+            default: 'Title Three'
+        },
+        columnOneBody: {
+            type: 'array',
+            source: 'children',
+            selector: '.ub_feature_one_body',
+            default: 'Gutenberg is really awesome! Ultimate Blocks makes it more awesome!'
+        },
+        columnTwoBody: {
+            type: 'array',
+            source: 'children',
+            selector: '.ub_feature_two_body',
+            default: 'Gutenberg is really awesome! Ultimate Blocks makes it more awesome!'
+        },
+        columnThreeBody: {
+            type: 'array',
+            source: 'children',
+            selector: '.ub_feature_three_body',
+            default: 'Gutenberg is really awesome! Ultimate Blocks makes it more awesome!'
+        },
+    },
 
 	/**
 	 * The edit function describes the structure of your block in the context of the editor.
@@ -63,28 +105,106 @@ registerBlockType( 'ub/feature-box', {
 	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
 	 */
 	edit: function( props ) {
+	    const {
+	        column,
+            columnOneTitle,
+            columnTwoTitle,
+            columnThreeTitle,
+            columnOneBody,
+            columnTwoBody,
+            columnThreeBody,
+        } = props.attributes;
+
+        const columns = [
+            { value: '1', label: __( 'One Column' ) },
+            { value: '2', label: __( 'Two Column' ) },
+            { value: '3', label: __( 'Three Column' ) },
+        ];
 		// Creates a <p class='wp-block-cgb-block-sample-block'></p>.
-		return (
+		return [
+
+
+            !! props.focus && (
+
+                <BlockControls key="controls"/>,
+
+                <InspectorControls key={ 'inspector' }>
+
+                    <SelectControl
+                        label={ __( 'Column Number' ) }
+                        value={ column }
+                        options={ columns.map( ({ value, label }) => ( {
+                            value: value,
+                            label: label,
+                        } ) ) }
+                        onChange={ ( value ) => { props.setAttributes( { column: value } ) } }
+                    />
+
+                </InspectorControls>
+            ),
+
 			<div className={ props.className }>
-                <div class="ub_feature_box column_3">
-                    <div class="ub_feature_1_3">
-                        <img src="http://www.gosugamers.net/uploads/images/avatars/members/146633-1410106710.jpg" alt=""/>
-                            <p className="ub_feature_one_title">Title</p>
-                            Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Curabitur ut rhon cus turpis.
+                <div className={`ub_feature_box column_${column}`}>
+                    <div class="ub_feature_1">
+                        <img src="http://materialdesignblog.com/wp-content/uploads/2015/11/1200x628.png" alt=""/>
+                        <RichText
+                            tagName="p"
+                            className="ub_feature_one_title"
+                            value={ columnOneTitle }
+                            onChange={ ( value ) => props.setAttributes( { columnOneTitle: value } ) }
+                            focus={ props.focus }
+                            keepPlaceholderOnFocus={true}
+                        />
+                        <RichText
+                            tagName="p"
+                            className="ub_feature_one_body"
+                            value={ columnOneBody }
+                            onChange={ ( value ) => props.setAttributes( { columnOneBody: value } ) }
+                            focus={ props.focus }
+                            keepPlaceholderOnFocus={true}
+                        />
                     </div>
-                    <div class="ub_feature_2_3">
-                        <img src="http://www.gosugamers.net/uploads/images/avatars/members/146633-1410106710.jpg" alt=""/>
-                            <p className="ub_feature_two_title">Title</p>
-                            Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Curabitur ut rhon cus turpis.
+                    <div class="ub_feature_2">
+                        <img src="http://materialdesignblog.com/wp-content/uploads/2015/11/1200x628.png" alt=""/>
+                        <RichText
+                            tagName="p"
+                            className="ub_feature_two_title"
+                            value={ columnTwoTitle }
+                            onChange={ ( value ) => props.setAttributes( { columnTwoTitle: value } ) }
+                            focus={ props.focus }
+                            keepPlaceholderOnFocus={true}
+                        />
+                        <RichText
+                            tagName="p"
+                            className="ub_feature_two_body"
+                            value={ columnTwoBody }
+                            onChange={ ( value ) => props.setAttributes( { columnTwoBody: value } ) }
+                            focus={ props.focus }
+                            keepPlaceholderOnFocus={true}
+                        />
                     </div>
-                    <div class="ub_feature_3_3">
-                        <img src="http://www.gosugamers.net/uploads/images/avatars/members/146633-1410106710.jpg" alt=""/>
-                            <p className="ub_feature_three_title">Title</p>
-                            Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Curabitur ut rhon cus turpis.
+                    <div class="ub_feature_3">
+                        <img src="http://materialdesignblog.com/wp-content/uploads/2015/11/1200x628.png" alt=""/>
+                        <RichText
+                            tagName="p"
+                            className="ub_feature_three_title"
+                            value={ columnThreeTitle }
+                            onChange={ ( value ) => props.setAttributes( { columnThreeTitle: value } ) }
+                            focus={ props.focus }
+                            keepPlaceholderOnFocus={true}
+                        />
+                        <RichText
+                            tagName="p"
+                            className="ub_feature_three_body"
+                            value={ columnThreeBody }
+                            onChange={ ( value ) => props.setAttributes( { columnThreeBody: value } ) }
+                            focus={ props.focus }
+                            keepPlaceholderOnFocus={true}
+                        />
                     </div>
                 </div>
 			</div>
-		);
+		];
 	},
 
 	/**
@@ -96,23 +216,33 @@ registerBlockType( 'ub/feature-box', {
 	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
 	 */
 	save: function( props ) {
+        const {
+            column,
+            columnOneTitle,
+            columnTwoTitle,
+            columnThreeTitle,
+            columnOneBody,
+            columnTwoBody,
+            columnThreeBody,
+        } = props.attributes;
+
 		return (
 			<div className={ props.className }>
-                <div class="ub_feature_box column_3">
-                    <div class="ub_feature_1_3">
-                        <img src="http://www.gosugamers.net/uploads/images/avatars/members/146633-1410106710.jpg" alt=""/>
-                        <p className="ub_feature_one_title">Title</p>
-                        Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Curabitur ut rhon cus turpis.
+                <div className={`ub_feature_box column_${column}`}>
+                    <div class="ub_feature_1">
+                        <img src="http://materialdesignblog.com/wp-content/uploads/2015/11/1200x628.png" alt=""/>
+                        <p className="ub_feature_one_title">{ columnOneTitle }</p>
+                        <p className="ub_feature_one_body">{ columnOneBody }</p>
                     </div>
-                    <div class="ub_feature_2_3">
-                        <img src="http://www.gosugamers.net/uploads/images/avatars/members/146633-1410106710.jpg" alt=""/>
-                        <p className="ub_feature_two_title">Title</p>
-                        Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Curabitur ut rhon cus turpis.
+                    <div class="ub_feature_2">
+                        <img src="http://materialdesignblog.com/wp-content/uploads/2015/11/1200x628.png" alt=""/>
+                        <p className="ub_feature_two_title">{ columnTwoTitle }</p>
+                        <p className="ub_feature_two_body">{ columnTwoBody }</p>
                     </div>
-                    <div class="ub_feature_3_3">
-                        <img src="http://www.gosugamers.net/uploads/images/avatars/members/146633-1410106710.jpg" alt=""/>
-                        <p className="ub_feature_three_title">Title</p>
-                        Lorem ipsum dolor sit amet, consec tetur adipiscing elit. Curabitur ut rhon cus turpis.
+                    <div class="ub_feature_3">
+                        <img src="http://materialdesignblog.com/wp-content/uploads/2015/11/1200x628.png" alt=""/>
+                        <p className="ub_feature_three_title">{ columnThreeTitle }</p>
+                        <p className="ub_feature_three_body">{ columnThreeBody }</p>
                     </div>
                 </div>
 			</div>
