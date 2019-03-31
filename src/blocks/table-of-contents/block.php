5ea19a20ce8050e7f35a581c746cd6bc1546263f
imtiazrayhan <irayhan.asif@gmail.com>
2019-03-31 18:49:07 -0400

    Conditional loading for assets

--- src/blocks/table-of-contents/block.php
+++ src/blocks/table-of-contents/block.php
@@ -6,7 +6,7 @@
  * @return void
  */
 function ub_table_of_contents_add_frontend_assets() {
-    if(has_block('ub/table-of-contents')){
+    if ( has_block( 'ub/table-of-contents' ) ) {
         wp_enqueue_script(
             'ultimate_blocks-table-of-contents-front-script',
             plugins_url( 'table-of-contents/front.js', dirname( __FILE__ ) ),
