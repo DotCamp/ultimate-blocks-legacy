50b175711b71dbb776628de939c9508e7a9d3f19
Alexius08 <jrsfac@gmail.com>
2019-06-07 01:16:35 +0800

    Convert Table of Contents frontend script to plain JavaScript

--- src/blocks/table-of-contents/front.js
+++ src/blocks/table-of-contents/front.js
@@ -1,17 +1,28 @@
-(function($) {
-	$(document).ready(function() {
-		const toggleButton = '.ub_table-of-contents-toggle-link';
+Array.from(
+	document.getElementsByClassName('ub_table-of-contents-toggle-link')
+).forEach(instance => {
+	let heightIsChecked = false;
+	let tocHeight = 0;
 
-		$(toggleButton).click(function() {
-			const block = $(this).closest('.ub_table-of-contents');
+	const block = instance.closest('.ub_table-of-contents');
+	const tocContainer = block.querySelector('.ub_table-of-contents-container');
 
-			block.find('.ub_table-of-contents-container').slideToggle();
-			if ($(this).html() === block.data('hidetext')) {
-				$(this).html(block.data('showtext'));
-			} else {
-				$(this).html(block.data('hidetext'));
-			}
-			return false; //needed to prevent scrolling
-		});
+	if (!heightIsChecked) {
+		const initialDisplayMode = tocContainer.style.display;
+		if (initialDisplayMode === 'none') {
+			tocContainer.style.display = 'block';
+			tocContainer.style.height = '';
+		}
+		tocHeight = tocContainer.offsetHeight;
+		tocContainer.style.height = `${
+			initialDisplayMode === 'none' ? 0 : tocHeight
+		}px`;
+		heightIsChecked = true;
+	}
+	instance.addEventListener('click', function(event) {
+		tocContainer.style.height = `${
+			tocContainer.style.height === '0px' ? tocHeight : '0'
+		}px`;
+		event.preventDefault();
 	});
-})(jQuery);
+});
