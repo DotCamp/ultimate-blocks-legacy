9206b78b2939e05ab61b731435869db8a31e478d
Alexius08 <jrsfac@gmail.com>
2019-07-05 01:07:12 +0800

    Release new image slider version

--- src/blocks/image-slider/block.js
+++ src/blocks/image-slider/block.js
@@ -378,74 +378,8 @@ registerBlockType('ub/image-slider', {
 			</div>
 		];
 	}),
-	save(props) {
-		const {
-			images,
-			isDraggable,
-			wrapsAround,
-			autoplays,
-			autoplayDuration,
-			sliderHeight,
-			showPageDots,
-			captions
-		} = props.attributes;
-
-		const imageArray = JSON.parse(images);
-		const captionArray = JSON.parse(captions);
-
-		return (
-			<div
-				className="ub_image_slider"
-				style={{
-					minHeight: `${25 +
-						(imageArray.length ? sliderHeight : 200)}px`,
-					display: 'block'
-				}}
-			>
-				<div
-					data-flickity={JSON.stringify({
-						draggable: isDraggable,
-						pageDots: showPageDots,
-						wrapAround: wrapsAround,
-						autoPlay: autoplays
-							? autoplayDuration * 1000
-							: autoplays,
-						adaptiveHeight: true
-					})}
-				>
-					{imageArray.map((c, i) => (
-						<div style={{ width: '100%' }}>
-							<img
-								key={i}
-								src={c.url}
-								style={{
-									display: 'block',
-									height: `${sliderHeight}px`,
-									objectFit: 'contain',
-									margin: '0 auto'
-								}}
-							/>
-							{captionArray[i].link !== '' ? (
-								<a
-									className="ub_image_silder_image_caption"
-									href={captionArray[i].link}
-								>
-									<RichText.Content
-										value={captionArray[i].text}
-									/>
-								</a>
-							) : (
-								<RichText.Content
-									tagName="span"
-									className="ub_image_silder_image_caption"
-									value={captionArray[i].text}
-								/>
-							)}
-						</div>
-					))}
-				</div>
-			</div>
-		);
+	save() {
+		return null;
 	},
 	deprecated: [{ attributes, save: version_1_1_4 }]
 });
--- src/blocks/image-slider/block.php
+++ src/blocks/image-slider/block.php
@@ -5,6 +5,73 @@
  *
  * @return void
  */
+
+function ub_render_image_slider_block($attributes){
+    extract($attributes);
+
+    $imageArray = json_decode($images, true);
+    $captionArray = json_decode($captions, true);
+
+    $gallery = '';
+    
+    foreach($imageArray as $key => $image){
+        $gallery .= '<div style="width: 100%">
+        <img src="'.$image['url'].'" style="display: block; height: '.
+            $sliderHeight.'px; object-fit: contain; margin: 0 auto;">'.
+            ($captionArray[$key]['link'] == '' ? '<span' : '<a href="'.$captionArray[$key]['link'].'"')
+            .' class="ub_image_slider_image_caption">'.$captionArray[$key]['text']
+            .($captionArray[$key]['link'] == '' ? '</span>' : '</a>').' </div>';
+    }
+
+    return '<div class="ub_image_slider" style="min-height: '.
+        (25+ (count($imageArray) > 0) ? $sliderHeight : 200 ).'px ; display: block;">
+        <div data-flickity='.json_encode(array('draggable'=>$isDraggable, 'pageDots'=> $showPageDots,
+            'wrapAround'=> $wrapsAround, 'autoPlay'=> ($autoplays ? $autoplayDuration * 1000 : $autoplays),
+            'adaptiveHeight'=>true )).'>'.$gallery
+            .'</div></div>';
+}
+
+function ub_register_image_slider_block(){
+    if ( function_exists( 'register_block_type' ) ) {
+        register_block_type('ub/image-slider', array(
+            'attributes' => array(
+                'images' => array(
+                    'type' => 'string',
+                    'default' => '[]'
+                ),
+                'captions' => array(
+                    'type' => 'string',
+                    'default' => '[]'
+                ),
+                'wrapsAround' => array(
+                    'type' => 'boolean',
+                    'default' => true
+                ),
+                'isDraggable' => array(
+                    'type' => 'boolean',
+                    'default' => false
+                ),
+                'autoplays' => array(
+                    'type' => 'boolean',
+                    'default' => false
+                ),
+                'autoplayDuration' => array(
+                    'type' => 'number',
+                    'default' => 3
+                ),
+                'sliderHeight' => array(
+                    'type' => 'number',
+                    'default' => 250
+                ),
+                'showPageDots' => array(
+                    'type' => 'boolean',
+                    'default' => true
+                )
+            ),
+            'render_callback' => 'ub_render_image_slider_block'));
+    }
+}
+
 function ub_image_slider_add_frontend_assets() {
     if ( has_block( 'ub/image-slider' ) ) {
         wp_enqueue_script(
@@ -16,4 +83,5 @@ function ub_image_slider_add_frontend_assets() {
     }
 }
 
+add_action('init', 'ub_register_image_slider_block');
 add_action( 'wp_enqueue_scripts', 'ub_image_slider_add_frontend_assets' );
