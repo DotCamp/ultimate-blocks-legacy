d7f2669d8335fa6bacab88055046c9c43fef708f
Alexius08 <jrsfac@gmail.com>
2019-07-04 22:02:32 +0800

    Ensure proper display of nested content toggle blocks
    Fix for one of the issues mentioned at https://wordpress.org/support/topic/accordion-inside-another-accordion/

--- src/blocks/content-toggle/front.js
+++ src/blocks/content-toggle/front.js
@@ -9,45 +9,66 @@ Array.from(
 
 	const panelContent = instance.nextSibling;
 
-	let heightIsChecked = false;
 	let panelHeight = 0;
 
-	if (!heightIsChecked) {
-		const initialHide =
-			panelContent.style.height === '0px' ||
-			panelContent.style.display === 'none';
-		if (initialHide) {
-			panelContent.style.height = '';
-			panelContent.style.paddingTop = '';
-			panelContent.style.paddingBottom = '';
-			panelContent.style.display = '';
-		}
-		panelHeight = panelContent.offsetHeight;
+	const initialHide =
+		panelContent.style.height === '0px' ||
+		panelContent.style.display === 'none';
+	if (initialHide) {
+		//temporarily show panel contents to enable taking panel height measurements
+		panelContent.style.height = '';
+		panelContent.style.paddingTop = '';
+		panelContent.style.paddingBottom = '';
+		panelContent.style.display = '';
+	}
+	panelHeight = panelContent.offsetHeight;
 
-		if (initialHide) {
-			panelContent.style.height = '0px';
-			panelContent.style.paddingTop = '0';
-			panelContent.style.paddingBottom = '0';
-			panelContent.style.marginTop = '0';
-			panelContent.style.marginBottom = '0';
-		} else {
-			panelContent.style.height = `${panelHeight}px`;
-		}
-
-		heightIsChecked = true;
+	if (initialHide) {
+		panelContent.style.height = '0px';
+		panelContent.style.paddingTop = '0';
+		panelContent.style.paddingBottom = '0';
+		panelContent.style.marginTop = '0';
+		panelContent.style.marginBottom = '0';
 	}
 
 	instance.addEventListener('click', function() {
+		if (
+			indicator.classList.contains('open') &&
+			panelHeight !== panelContent.offsetHeight
+		) {
+			panelHeight = panelContent.offsetHeight;
+		}
+
+		if (panelContent.style.height === '') {
+			panelContent.style.height = `${panelHeight}px`;
+		}
 		panelContent.style.transition = 'all 0.5s ease-in-out';
+
 		indicator.classList.toggle('open');
-		panelContent.style.height = `${
-			indicator.classList.contains('open') ? panelHeight : 0
-		}px`;
+	});
+
+	indicator.addEventListener('transitionstart', function() {
+		if (
+			!indicator.classList.contains('open') &&
+			panelHeight !== panelContent.offsetHeight
+		) {
+			panelHeight = panelContent.offsetHeight;
+		}
 
 		const newVal = indicator.classList.contains('open') ? '' : '0';
 		panelContent.style.paddingTop = newVal;
 		panelContent.style.paddingBottom = newVal;
 		panelContent.style.marginTop = newVal;
 		panelContent.style.marginBottom = newVal;
+		panelContent.style.height = `${
+			indicator.classList.contains('open') ? panelHeight : 0
+		}px`;
+	});
+
+	panelContent.addEventListener('transitionend', function() {
+		panelContent.style.transition = '';
+		if (panelContent.style.height !== '0px') {
+			panelContent.style.height = '';
+		}
 	});
 });
