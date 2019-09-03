a0a078a9234bc7aa3c5757b6ad4d63a5fb8dd040
Alexius08 <jrsfac@gmail.com>
2019-09-03 17:14:02 +0800

    Remove unnecessary dependencies when enqueueing Image Slider

--- src/blocks/image-slider/block.php
+++ src/blocks/image-slider/block.php
@@ -45,7 +45,7 @@ function ub_image_slider_add_frontend_assets() {
         wp_enqueue_script(
             'ultimate_blocks-image-slider-front-script',
             plugins_url( '/flickity.pkgd.js', __FILE__ ),
-            [ 'wp-blocks', 'wp-element', 'wp-components', 'wp-i18n' ],
+            array(),
             Ultimate_Blocks_Constants::plugin_version()
         );
     }
