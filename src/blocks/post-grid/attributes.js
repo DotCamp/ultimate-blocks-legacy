61d116646a035ec6978840014b60d78be5ec61b5
Alexius08 <jrsfac@gmail.com>
2019-12-13 06:30:33 +0800

    Ensure boolean values for boolean Post Grid attributes

--- src/blocks/post-grid/attributes.js
+++ src/blocks/post-grid/attributes.js
@@ -1,64 +1,64 @@
 const attributes = {
-       checkPostImage: {
-           type: 'boolean',
-           default: 'true'
-       },
-       checkPostAuthor: {
-           type: 'boolean',
-           default: 'true'
-       },
-       checkPostDate: {
-           type: 'boolean',
-           default: 'true'
-       },
-       checkPostExcerpt: {
-           type: 'boolean',
-           default: 'true'
-       },
-       excerptLength: {
-           type: 'number',
-           default: 55
-       },
-       checkPostLink: {
-           type: 'bolean',
-           default: 'true',
-       },
-       readMoreText: {
-           type: 'string',
-           default: 'Continue Reading'
-       },
-       amountPosts: {
-           type: 'number',
-           default: 6,
-       },
-       postLayout: {
-           type: 'string',
-           default: 'grid'
-       },
-       columns: {
-           type: 'number',
-           default: 2
-       },
-       categories: {
-           type: 'string',
-           value: ''
-       },
-       postType: {
-           type: 'string',
-           default: 'post'
-       },
-       offset: {
-           type: 'number',
-           value: 0
-       },
-       order: {
-           type: 'string',
-           value: 'desc'
-       },
-       orderBy: {
-           type: 'string',
-           value: 'date'
-       }
+	checkPostImage: {
+		type: "boolean",
+		default: true
+	},
+	checkPostAuthor: {
+		type: "boolean",
+		default: true
+	},
+	checkPostDate: {
+		type: "boolean",
+		default: true
+	},
+	checkPostExcerpt: {
+		type: "boolean",
+		default: true
+	},
+	excerptLength: {
+		type: "number",
+		default: 55
+	},
+	checkPostLink: {
+		type: "bolean",
+		default: true
+	},
+	readMoreText: {
+		type: "string",
+		default: "Continue Reading"
+	},
+	amountPosts: {
+		type: "number",
+		default: 6
+	},
+	postLayout: {
+		type: "string",
+		default: "grid"
+	},
+	columns: {
+		type: "number",
+		default: 2
+	},
+	categories: {
+		type: "string",
+		value: ""
+	},
+	postType: {
+		type: "string",
+		default: "post"
+	},
+	offset: {
+		type: "number",
+		value: 0
+	},
+	order: {
+		type: "string",
+		value: "desc"
+	},
+	orderBy: {
+		type: "string",
+		value: "date"
+	}
 };
 
 export default attributes;
