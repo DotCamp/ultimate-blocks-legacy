53b8c6d598ae1612a614822e7f446d78113a1129
Alexius08 <jrsfac@gmail.com>
2019-12-15 05:45:43 +0800

    Make the built-in block manager work again

--- dist/deactivator.build.js
+++ dist/deactivator.build.js
@@ -1,14 +1,9 @@
 /* eslint-disable */
 
 if (window.ultimate_blocks) {
-
-	window.ultimate_blocks.forEach( block => {
-		if(!block.active) {
-			var in_use = $('.wp-block-' + block.name.replace('/', '-')).length > 0;
-			if(!in_use) {
-				wp.blocks.unregisterBlockType(block.name);
-			}
+	window.ultimate_blocks.forEach(block => {
+		if (!block.active) {
+			wp.blocks.unregisterBlockType(block.name);
 		}
 	});
-
 }
