bc34787333f43a1506c9ad6706c990c0a14bcd43
Alexius08 <jrsfac@gmail.com>
2020-03-30 01:18:17 +0800

    Fix error with image slider frontend script

--- src/blocks/image-slider/front.js
+++ src/blocks/image-slider/front.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
 	Array.prototype.slice
 		.call(document.getElementsByClassName("ub_image_slider"))
 		.forEach(instance => {
-			Flickity.data(instance.querySelector("[data-flickity]")).resize();
+			if (instance.querySelector("[data-flickity]")) {
+				Flickity.data(instance.querySelector("[data-flickity]")).resize();
+			}
 		});
 });
