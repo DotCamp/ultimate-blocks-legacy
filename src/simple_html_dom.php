449dc98c53cc3231de30ec17ad147560e02c1151
Alexius08 <jrsfac@gmail.com>
2020-04-23 22:41:43 +0800

    Update Simple HTML DOM to v1.9.1 and prevent variable naming conflicts

--- src/simple_html_dom.php
+++ src/simple_html_dom.php
@@ -37,32 +37,32 @@
  *   Vadim Voituk
  *   Antcs
  *
- * Version Rev. 1.9 (290)
+ * Version Rev. 1.9.1 (291)
  */
 
-define('HDOM_TYPE_ELEMENT', 1);
-define('HDOM_TYPE_COMMENT', 2);
-define('HDOM_TYPE_TEXT', 3);
-define('HDOM_TYPE_ENDTAG', 4);
-define('HDOM_TYPE_ROOT', 5);
-define('HDOM_TYPE_UNKNOWN', 6);
-define('HDOM_QUOTE_DOUBLE', 0);
-define('HDOM_QUOTE_SINGLE', 1);
-define('HDOM_QUOTE_NO', 3);
-define('HDOM_INFO_BEGIN', 0);
-define('HDOM_INFO_END', 1);
-define('HDOM_INFO_QUOTE', 2);
-define('HDOM_INFO_SPACE', 3);
-define('HDOM_INFO_TEXT', 4);
-define('HDOM_INFO_INNER', 5);
-define('HDOM_INFO_OUTER', 6);
-define('HDOM_INFO_ENDSPACE', 7);
+defined('HDOM_TYPE_ELEMENT') || define('HDOM_TYPE_ELEMENT', 1);
+defined('HDOM_TYPE_COMMENT') || define('HDOM_TYPE_COMMENT', 2);
+defined('HDOM_TYPE_TEXT') || define('HDOM_TYPE_TEXT', 3);
+defined('HDOM_TYPE_ENDTAG') || define('HDOM_TYPE_ENDTAG', 4);
+defined('HDOM_TYPE_ROOT') || define('HDOM_TYPE_ROOT', 5);
+defined('HDOM_TYPE_UNKNOWN') || define('HDOM_TYPE_UNKNOWN', 6);
+defined('HDOM_QUOTE_DOUBLE') || define('HDOM_QUOTE_DOUBLE', 0);
+defined('HDOM_QUOTE_SINGLE') || define('HDOM_QUOTE_SINGLE', 1);
+defined('HDOM_QUOTE_NO') || define('HDOM_QUOTE_NO', 3);
+defined('HDOM_INFO_BEGIN') || define('HDOM_INFO_BEGIN', 0);
+defined('HDOM_INFO_END') || define('HDOM_INFO_END', 1);
+defined('HDOM_INFO_QUOTE') || define('HDOM_INFO_QUOTE', 2);
+defined('HDOM_INFO_SPACE') || define('HDOM_INFO_SPACE', 3);
+defined('HDOM_INFO_TEXT') || define('HDOM_INFO_TEXT', 4);
+defined('HDOM_INFO_INNER') || define('HDOM_INFO_INNER', 5);
+defined('HDOM_INFO_OUTER') || define('HDOM_INFO_OUTER', 6);
+defined('HDOM_INFO_ENDSPACE') || define('HDOM_INFO_ENDSPACE', 7);
 
 defined('DEFAULT_TARGET_CHARSET') || define('DEFAULT_TARGET_CHARSET', 'UTF-8');
 defined('DEFAULT_BR_TEXT') || define('DEFAULT_BR_TEXT', "\r\n");
 defined('DEFAULT_SPAN_TEXT') || define('DEFAULT_SPAN_TEXT', ' ');
 defined('MAX_FILE_SIZE') || define('MAX_FILE_SIZE', 600000);
-define('HDOM_SMARTY_AS_TEXT', 1);
+defined('HDOM_SMARTY_AS_TEXT') || define('HDOM_SMARTY_AS_TEXT', 1);
 
 function file_get_html(
 	$url,
@@ -626,6 +626,13 @@ class simple_html_dom_node
 			// Skip root nodes
 			if(!$node->parent) {
 				$pass = false;
+			}
+
+			// Handle 'text' selector
+			if($pass && $tag === 'text' && $node->tag === 'text') {
+				$ret[array_search($node, $this->dom->nodes, true)] = 1;
+				unset($node);
+				continue;
 			}
 
 			// Skip if node isn't a child node (i.e. text nodes)
