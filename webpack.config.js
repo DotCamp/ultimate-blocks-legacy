0344643e32d844200d1f81954239aa622bf3ad8c
Alexius08 <jrsfac@gmail.com>
2020-11-01 10:51:03 +0800

    Prevent automatic deletion of block deactivator script

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 const defaultConfig = require("@wordpress/scripts/config/webpack.config.js");
 const path = require("path");
 const IgnoreEmitPlugin = require("ignore-emit-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
 	...defaultConfig,
 	entry: {
-		index: path.resolve(process.cwd(), "src", "blocks.js")
+		index: path.resolve(process.cwd(), "src", "blocks.js"),
 	},
 	plugins: [
-		...defaultConfig.plugins,
-		new IgnoreEmitPlugin(["blocks.build.asset.php", "blocks.build.js.map"])
+		...defaultConfig.plugins.filter((p) => !(p instanceof CleanWebpackPlugin)),
+		new IgnoreEmitPlugin(["blocks.build.asset.php", "blocks.build.js.map"]),
 	],
 	output: {
-		filename: "blocks.build.js"
-	}
+		filename: "blocks.build.js",
+	},
 };
