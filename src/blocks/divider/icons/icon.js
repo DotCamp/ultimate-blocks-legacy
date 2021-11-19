33cf54b5e3eb123f5b6e3b0ebc7a36d63e24ec80
Alexius08 <jrsfac@gmail.com>
2021-11-19 21:42:07 +0800

    Cleanup SVG icons

--- src/blocks/divider/icons/icon.js
+++ src/blocks/divider/icons/icon.js
@@ -1,6 +1,12 @@
-const icon = <svg xmlns="http://www.w3.org/2000/svg" id="Capa_1" viewBox="0 0 60 60"
-                  width="20" height="20">
-    <path d="M0,0v60h60V0H0z M51,32H9v-4h42V32z" fill="#f63d3d" />
-</svg>;
+const icon = (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 60 60"
+		width="20"
+		height="20"
+	>
+		<path d="M0,0v60h60V0H0z M51,32H9v-4h42V32z" fill="#f63d3d" />
+	</svg>
+);
 
 export default icon;
