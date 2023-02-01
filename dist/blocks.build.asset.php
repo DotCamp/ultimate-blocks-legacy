fdabc8993b2232ab4f85d0ab7e93cf3fb283030e
Erdem Bircan <erdem.erdembircan@gmail.com>
2023-02-02 01:32:32 +0300

    merge build

--- dist/blocks.build.asset.php
+++ dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e1f9543a00a7f7743d2a');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '42f33dd607ed68de256e');
