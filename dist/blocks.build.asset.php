a3e440e78a420d4ec9e366b41e9ee333fb780cb0
Alexius08 <jrsfac@gmail.com>
2023-02-19 02:59:20 +0800

    Update FontAwesome to v6.3

--- dist/blocks.build.asset.php
+++ dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '42f33dd607ed68de256e');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7907a02948f2ea18068b');
