1dd222bcbea95c04dddcff47c967a8a9fef6fcc7
Erdem Bircan <erdem.erdembircan@gmail.com>
2023-03-13 13:05:44 +0300

    library update

--- dist/blocks.build.asset.php
+++ dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5971eaf03ad0f20b2f11');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ae6732b5d5f4984799e0');
