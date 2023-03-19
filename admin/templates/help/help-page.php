90bf50c759a1bc5c318f9cedd9171572de8f25e8
Imtiaz Rayhan <irayhan.asif@gmail.com>
2023-03-19 18:17:26 -0400

    Welcome page updates

--- admin/templates/help/help-page.php
+++ admin/templates/help/help-page.php
@@ -16,10 +16,14 @@
 <div class="ub-container">
     <div class="ultimate-blocks">
         <h1><?php esc_html_e( 'Welcome to Ultimate Blocks!', 'ultimate-blocks' ); ?></h1>
-        <h5><?php esc_html_e( 'Essential Collection of Blocks for Gutenberg Editor', 'ultimate-blocks' ); ?></h5>
+        <h5><?php esc_html_e( 'Essential Collection of Blocks for Block Editor', 'ultimate-blocks' ); ?></h5>
     </div>
     <div class="ub-intro">
-        <p><?php esc_html_e( 'Ultimate Blocks comes with the essential Gutenberg blocks you need to create better content with Gutenberg.', 'ultimate-blocks' ); ?></p>
+        <p style="font-size: 25px; font-weight: bold"><?php esc_html_e( "Ultimate Blocks plugin makes building content in WordPress a breeze!", 'ultimate-blocks' ); ?></p>
+		<p><?php esc_html_e( "With its cool customizable blocks, like table of contents, tabbed content,", 'ultimate-blocks');?><br>
+		<?php esc_html_e( "content toggle and buttons, you can create engaging and attractive content in no time.", "ultimate-blocks" ); ?></p>
+		<p><?php esc_html_e( "Plus, it's SEO-friendly, so your content stays fresh and easy to find.", "ultimate-blocks");?></p>
+		<p><?php esc_html_e( "Say hello to a better online presence with Ultimate Blocks!", "ultimate-blocks");?></p>
         <h2><span class="color-red"><?php esc_html_e( '22 awesome blocks.', 'ultimate-blocks' ); ?></span> <?php esc_html_e( 'More in the making...', 'ultimate-blocks' ); ?></h2>
         <div class="ub-button-wrap ultimate-blocks-clear">
             <div class="left">
@@ -35,6 +39,17 @@
             </div>
         </div>
     </div>
+</div>
+
+<div class="ub-container">
+	<div class="ub-social-intro" style="min-height: 250px;">
+		<h2><?php esc_html_e( 'Get Our Free WordPress Block Theme', 'ultimate-blocks' ); ?></h2>
+		<p><?php esc_html_e( "GroundWP is a free, lightweight WordPress theme centered around block patterns. Its blazing-fast speed and block-based design let you create stunning, responsive websites with ease. Experience seamless customization and top-notch performance with GroundWP!", "ultimate-block"); ?></p>
+		
+		<img src="https://i0.wp.com/themes.svn.wordpress.org/groundwp/1.5.5/screenshot.png" style="max-width: 100%" alt="" srcset="">
+		<a target="_blank" href="https://wordpress.org/themes/groundwp/" style="display: block;width: 300px;margin: 50px auto 10px auto;" class="ultimate-blocks-btn ultimate-blocks-btn-block ultimate-blocks-btn-lg ultimate-blocks-btn-red"><?php esc_html_e( 'Download GroundWP Free!', 'ultimate-blocks' ); ?></a>
+		<h2><?php esc_html_e( 'Combine Ultimate Blocks With GroundWP to Create Better Content and Better Website!', 'ultimate-blocks' ); ?></h2>
+	</div>
 </div>
 
 <div class="ub-container">
@@ -57,14 +72,3 @@
     </div>
 </div>
 
-<div class="ub-container">
-	<div class="ub-social-intro" style="min-height: 250px;">
-		<h2><?php esc_html_e( 'Other Plugins From Us', 'ultimate-blocks' ); ?></h2>
-		<div class="left">
-			<p style="text-align: left;"><a href="https://wordpress.org/plugins/wp-table-builder/" target="_blank"><?php esc_html_e( 'WP Table Builder', 'ultimate-blocks' ); ?></a> - Drag & drop responsive table builder.</p>		
-		</div>
-		<div class="right">
-			<p style="text-align: left;"><a href="https://wordpress.org/plugins/wp-coupons-and-deals/" target="_blank""><?php esc_html_e( 'WP Coupons and Deals', 'ultimate-blocks' ); ?></a> Coupons & deals plugin for affiliate marketers.</p>
-		</div>
-	</div>
-</div>
