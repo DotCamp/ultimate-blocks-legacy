e6158d445fb14f406f997abc68f290d2d10d136d
Erdem Bircan <erdem.erdembircan@gmail.com>
2023-04-06 17:02:22 +0300

    dev build

--- dist/priority.build.asset.php
+++ dist/priority.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-hooks', 'wp-i18n'), 'version' => 'ab2954fad8e93a530e9c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-hooks', 'wp-i18n'), 'version' => 'c735867a12735536a207');
