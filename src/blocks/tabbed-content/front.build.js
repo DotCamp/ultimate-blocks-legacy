76f8eeb453c72d40079d97c8f9eeafce5a9cb3cd
Alexius08 <jrsfac@gmail.com>
2023-04-11 08:39:14 +0800

    Fix issue with undefined tab anchor in tabbed content

--- src/blocks/tabbed-content/front.build.js
+++ src/blocks/tabbed-content/front.build.js
@@ -97,9 +97,12 @@ function ub_handleTabEvent(tab) {
         embeddedContent.style.removeProperty("width");
         embeddedContent.style.removeProperty("height");
       });
-      history.replaceState({
-        page: history.length
-      }, document.getElementsByTagName("title")[0].innerHTML, "#".concat(tabContent.dataset.tabAnchor)); //doesn't trigger window.onhashchange
+
+      if (tabContent.dataset.tabAnchor) {
+        history.replaceState({
+          page: history.length
+        }, document.getElementsByTagName("title")[0].innerHTML, "#".concat(tabContent.dataset.tabAnchor)); //doesn't trigger window.onhashchange
+      }
     } else {
       tabContent.classList.remove("active");
       tabContent.classList.add("ub-hide");
