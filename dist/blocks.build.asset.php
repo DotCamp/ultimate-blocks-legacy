96b247a0780e4ffead1bbab381fbbc374410eadd
Alexius08 <jrsfac@gmail.com>
2023-04-14 22:54:58 +0800

    Split howto inspector controls

--- dist/blocks.build.asset.php
+++ dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e7dbc8e305152e66ad73');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9b30cbc69ad73b7e0891');
--- src/blocks/how-to/components.js
+++ src/blocks/how-to/components.js
@@ -83,196 +83,203 @@ function InspectorPanel(props) {
 	}
 
 	return (
-		<InspectorControls>
-			<PanelBody title={__("General")}>
-				<ToggleControl
-					label={__("Use sections")}
-					checked={useSections}
-					onChange={(useSections) => {
-						setAttributes({ useSections });
-						if (useSections) {
-							let newSection = JSON.parse(JSON.stringify(section));
-							newSection.forEach((ns, i) =>
-								ns.steps.forEach((s, j) => {
-									s.anchor = `section${i}step${j}`;
-								})
-							);
-							if (currentStep !== "") {
-								updateState({ currentStep: `section-0-${currentStep}` });
-							}
-						} else {
-							updateState({
-								currentStep: currentStep.slice(currentStep.indexOf("step")),
-							});
-							if (section.length < 1) {
-								setAttributes({
-									section: [{ sectionName: "", steps: [] }],
-								});
-							} else {
+		<>
+			<InspectorControls group="settings">
+				<PanelBody title={__("General")}>
+					<ToggleControl
+						label={__("Use sections")}
+						checked={useSections}
+						onChange={(useSections) => {
+							setAttributes({ useSections });
+							if (useSections) {
 								let newSection = JSON.parse(JSON.stringify(section));
-								newSection[0].steps.forEach((s, i) => {
-									s.anchor = `step${i}`;
-								});
-								setAttributes({ section: newSection });
-							}
-						}
-					}}
-				/>
-				<ToggleControl
-					label={__("Use additional recommended attributes")}
-					checked={advancedMode}
-					onChange={(advancedMode) => setAttributes({ advancedMode })}
-				/>
-				{advancedMode && (
-					<>
-						<ToggleControl
-							label={__("Include list of supplies")}
-							checked={includeSuppliesList}
-							onChange={(includeSuppliesList) =>
-								setAttributes({ includeSuppliesList })
-							}
-						/>
-						<ToggleControl
-							label={__("Include list of tools")}
-							checked={includeToolsList}
-							onChange={(includeToolsList) =>
-								setAttributes({ includeToolsList })
-							}
-						/>
-						<ToggleControl
-							label={__("Display the unit first in cost")}
-							checked={showUnitFirst}
-							onChange={(showUnitFirst) => setAttributes({ showUnitFirst })}
-						/>
-					</>
-				)}
-				{useSections && (
-					<RadioControl
-						label={__("Section list style")}
-						selected={sectionListStyle}
-						options={["none", "ordered", "unordered"].map((a) => ({
-							label: __(a),
-							value: a,
-						}))}
-						onChange={(sectionListStyle) => setAttributes({ sectionListStyle })}
-					/>
-				)}
-			</PanelBody>
-			{activeImage.width > 0 && (
-				<PanelBody title={__("Desktop image display settings")}>
-					<RangeControl
-						label={__("Image width")}
-						value={activeImage.width}
-						onChange={(imageWidth) => {
-							if (currentStep === "final") {
-								setAttributes({ finalImageWidth: imageWidth });
-							} else {
-								const parsed = currentStep.split("-");
-								let sectionClone = JSON.parse(JSON.stringify(section));
-								if (useSections) {
-									sectionNum = parseInt(parsed[1]);
-									stepNum = parseInt(parsed[3]);
-								} else {
-									stepNum = parseInt(parsed[1]);
+								newSection.forEach((ns, i) =>
+									ns.steps.forEach((s, j) => {
+										s.anchor = `section${i}step${j}`;
+									})
+								);
+								if (currentStep !== "") {
+									updateState({ currentStep: `section-0-${currentStep}` });
 								}
-								sectionClone[Math.max(sectionNum, 0)].steps[
-									stepNum
-								].stepPic.width = imageWidth;
-
-								setAttributes({ section: sectionClone });
+							} else {
+								updateState({
+									currentStep: currentStep.slice(currentStep.indexOf("step")),
+								});
+								if (section.length < 1) {
+									setAttributes({
+										section: [{ sectionName: "", steps: [] }],
+									});
+								} else {
+									let newSection = JSON.parse(JSON.stringify(section));
+									newSection[0].steps.forEach((s, i) => {
+										s.anchor = `step${i}`;
+									});
+									setAttributes({ section: newSection });
+								}
 							}
 						}}
-						min={200}
-						max={800}
+					/>
+					<ToggleControl
+						label={__("Use additional recommended attributes")}
+						checked={advancedMode}
+						onChange={(advancedMode) => setAttributes({ advancedMode })}
+					/>
+					{advancedMode && (
+						<>
+							<ToggleControl
+								label={__("Include list of supplies")}
+								checked={includeSuppliesList}
+								onChange={(includeSuppliesList) =>
+									setAttributes({ includeSuppliesList })
+								}
+							/>
+							<ToggleControl
+								label={__("Include list of tools")}
+								checked={includeToolsList}
+								onChange={(includeToolsList) =>
+									setAttributes({ includeToolsList })
+								}
+							/>
+							<ToggleControl
+								label={__("Display the unit first in cost")}
+								checked={showUnitFirst}
+								onChange={(showUnitFirst) => setAttributes({ showUnitFirst })}
+							/>
+						</>
+					)}
+					{useSections && (
+						<RadioControl
+							label={__("Section list style")}
+							selected={sectionListStyle}
+							options={["none", "ordered", "unordered"].map((a) => ({
+								label: __(a),
+								value: a,
+							}))}
+							onChange={(sectionListStyle) =>
+								setAttributes({ sectionListStyle })
+							}
+						/>
+					)}
+				</PanelBody>
+
+				{advancedMode && includeSuppliesList && (
+					<PanelBody title={__("Supplies List")}>
+						<ToggleControl
+							label={__("Enable adding image for each supply")}
+							checked={addSupplyImages}
+							onChange={(addSupplyImages) => setAttributes({ addSupplyImages })}
+						/>
+						<RadioControl
+							label={__("Supplies list style")}
+							selected={suppliesListStyle}
+							options={["none", "ordered", "unordered"].map((a) => ({
+								label: __(a),
+								value: a,
+							}))}
+							onChange={(suppliesListStyle) =>
+								setAttributes({ suppliesListStyle })
+							}
+						/>
+					</PanelBody>
+				)}
+				{advancedMode && includeToolsList && (
+					<PanelBody title={__("Tools List")}>
+						<ToggleControl
+							label={__("Enable adding image for each tool")}
+							checked={addToolImages}
+							onChange={(addToolImages) => setAttributes({ addToolImages })}
+						/>
+						<RadioControl
+							label={__("Tools list style")}
+							selected={toolsListStyle}
+							options={["none", "ordered", "unordered"].map((a) => ({
+								label: __(a),
+								value: a,
+							}))}
+							onChange={(toolsListStyle) => setAttributes({ toolsListStyle })}
+						/>
+					</PanelBody>
+				)}
+				<PanelBody title={__("Title Tags")}>
+					<SelectControl
+						label={__("HowTo title tag")}
+						value={firstLevelTag}
+						options={tagList.map((tag) => ({ label: __(tag), value: tag }))}
+						onChange={(firstLevelTag) => setAttributes({ firstLevelTag })}
 					/>
 					<SelectControl
-						label={__("Image float")}
-						value={activeImage.float}
-						onChange={(newFloatValue) => {
-							if (currentStep === "final") {
-								setAttributes({ finalImageFloat: newFloatValue });
-							} else {
-								const parsed = currentStep.split("-");
-								let sectionClone = JSON.parse(JSON.stringify(section));
-								if (useSections) {
-									sectionNum = parseInt(parsed[1]);
-									stepNum = parseInt(parsed[3]);
+						label={__("Section title tag")}
+						value={secondLevelTag}
+						options={tagList.map((tag) => ({ label: __(tag), value: tag }))}
+						onChange={(secondLevelTag) => setAttributes({ secondLevelTag })}
+					/>
+					<SelectControl
+						label={__("Step title tag")}
+						value={thirdLevelTag}
+						options={tagList.map((tag) => ({ label: __(tag), value: tag }))}
+						onChange={(thirdLevelTag) => setAttributes({ thirdLevelTag })}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<InspectorControls group="styles">
+				{activeImage.width > 0 && (
+					<PanelBody title={__("Desktop image display settings")}>
+						<RangeControl
+							label={__("Image width")}
+							value={activeImage.width}
+							onChange={(imageWidth) => {
+								if (currentStep === "final") {
+									setAttributes({ finalImageWidth: imageWidth });
 								} else {
-									stepNum = parseInt(parsed[1]);
-								}
-								sectionClone[Math.max(sectionNum, 0)].steps[
-									stepNum
-								].stepPic.float = newFloatValue;
+									const parsed = currentStep.split("-");
+									let sectionClone = JSON.parse(JSON.stringify(section));
+									if (useSections) {
+										sectionNum = parseInt(parsed[1]);
+										stepNum = parseInt(parsed[3]);
+									} else {
+										stepNum = parseInt(parsed[1]);
+									}
+									sectionClone[Math.max(sectionNum, 0)].steps[
+										stepNum
+									].stepPic.width = imageWidth;
 
-								setAttributes({ section: sectionClone });
-							}
-						}}
-						options={["none", "left", "right"].map((a) => ({
-							label: a,
-							value: a,
-						}))}
-					/>
-				</PanelBody>
-			)}
-			{advancedMode && includeSuppliesList && (
-				<PanelBody title={__("Supplies List")}>
-					<ToggleControl
-						label={__("Enable adding image for each supply")}
-						checked={addSupplyImages}
-						onChange={(addSupplyImages) => setAttributes({ addSupplyImages })}
-					/>
-					<RadioControl
-						label={__("Supplies list style")}
-						selected={suppliesListStyle}
-						options={["none", "ordered", "unordered"].map((a) => ({
-							label: __(a),
-							value: a,
-						}))}
-						onChange={(suppliesListStyle) =>
-							setAttributes({ suppliesListStyle })
-						}
-					/>
-				</PanelBody>
-			)}
-			{advancedMode && includeToolsList && (
-				<PanelBody title={__("Tools List")}>
-					<ToggleControl
-						label={__("Enable adding image for each tool")}
-						checked={addToolImages}
-						onChange={(addToolImages) => setAttributes({ addToolImages })}
-					/>
-					<RadioControl
-						label={__("Tools list style")}
-						selected={toolsListStyle}
-						options={["none", "ordered", "unordered"].map((a) => ({
-							label: __(a),
-							value: a,
-						}))}
-						onChange={(toolsListStyle) => setAttributes({ toolsListStyle })}
-					/>
-				</PanelBody>
-			)}
-			<PanelBody title={__("Title Tags")}>
-				<SelectControl
-					label={__("HowTo title tag")}
-					value={firstLevelTag}
-					options={tagList.map((tag) => ({ label: __(tag), value: tag }))}
-					onChange={(firstLevelTag) => setAttributes({ firstLevelTag })}
-				/>
-				<SelectControl
-					label={__("Section title tag")}
-					value={secondLevelTag}
-					options={tagList.map((tag) => ({ label: __(tag), value: tag }))}
-					onChange={(secondLevelTag) => setAttributes({ secondLevelTag })}
-				/>
-				<SelectControl
-					label={__("Step title tag")}
-					value={thirdLevelTag}
-					options={tagList.map((tag) => ({ label: __(tag), value: tag }))}
-					onChange={(thirdLevelTag) => setAttributes({ thirdLevelTag })}
-				/>
-			</PanelBody>
-		</InspectorControls>
+									setAttributes({ section: sectionClone });
+								}
+							}}
+							min={200}
+							max={800}
+						/>
+						<SelectControl
+							label={__("Image float")}
+							value={activeImage.float}
+							onChange={(newFloatValue) => {
+								if (currentStep === "final") {
+									setAttributes({ finalImageFloat: newFloatValue });
+								} else {
+									const parsed = currentStep.split("-");
+									let sectionClone = JSON.parse(JSON.stringify(section));
+									if (useSections) {
+										sectionNum = parseInt(parsed[1]);
+										stepNum = parseInt(parsed[3]);
+									} else {
+										stepNum = parseInt(parsed[1]);
+									}
+									sectionClone[Math.max(sectionNum, 0)].steps[
+										stepNum
+									].stepPic.float = newFloatValue;
+
+									setAttributes({ section: sectionClone });
+								}
+							}}
+							options={["none", "left", "right"].map((a) => ({
+								label: a,
+								value: a,
+							}))}
+						/>
+					</PanelBody>
+				)}
+			</InspectorControls>
+		</>
 	);
 }
 
