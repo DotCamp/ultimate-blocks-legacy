dbab64571192bdd6853023a240abffb578109e3e
Erdem Bircan <erdem.erdembircan@gmail.com>
2023-07-17 16:45:08 +0300

    icon block invalid DOM property fix

--- dist/blocks.build.asset.php
+++ dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '653cfff074153b21db2a');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '87018c0dfa3e0eb3a97e');
--- src/blocks/icon/icons/block-icon.js
+++ src/blocks/icon/icons/block-icon.js
@@ -15,7 +15,7 @@ export const blockIcon = (
 		viewBox="0 0 512 512"
 		style={{ enableBackground: "new 0 0 512 512" }}
 		xmlSpace="preserve"
-		class=""
+		className=""
 	>
 		<G>
 			<G data-name="Layer 2">
@@ -26,7 +26,7 @@ export const blockIcon = (
 						r="256"
 						fill="#e11b4c"
 						data-original="#252a3f"
-						class=""
+						className=""
 						opacity="1"
 					></Path>
 					<Circle
@@ -41,7 +41,7 @@ export const blockIcon = (
 						d="M150.64 277.7V157.22q0-15.37 6.92-23.07t18.2-7.69q11.79 0 18.72 7.69t6.92 23.07v123.22q0 21 4.7 35.12a40.47 40.47 0 0 0 16.66 21.87q12 7.79 33.5 7.78 29.73 0 42-15.81t12.3-47.94V157.22q0-15.56 6.84-23.16t18.28-7.6q11.46 0 18.55 7.6t7.09 23.16V277.7q0 29.4-5.73 49.05A75.72 75.72 0 0 1 334 361.27a83.92 83.92 0 0 1-31.79 18.46q-18.12 5.81-42.38 5.81-28.89 0-49.74-6.24T176.1 360q-13.15-13.07-19.31-33.49t-6.15-48.81z"
 						data-name="U"
 						data-original="#252a3f"
-						class=""
+						className=""
 						opacity="1"
 					></Path>
 				</G>
