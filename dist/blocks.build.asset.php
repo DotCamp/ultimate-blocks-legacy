22a85c05240b426b79756f733675b0492c3476f0
Erdem Bircan <erdem.erdembircan@gmail.com>
2023-07-18 15:23:41 +0300

    advanced video logo color updates

--- dist/blocks.build.asset.php
+++ dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '203e0e39be98d3d48c89');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'dcad3583b6c2e3f59e13');
