b7ca5634b5d9fd7cbcf8fcc72dba08c4d473ad2e
Erdem Bircan <erdem.erdembircan@gmail.com>
2023-08-17 12:53:15 +0300

    styled-box block pattern fix

--- dist/blocks.build.asset.php
+++ dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '80f2ef8b003dca17a1bf');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'c6af9faaaddfb616b9a7');
