08e28e3de5750cebaae09bf9453daec99dc93752
Erdem Bircan <erdem.erdembircan@gmail.com>
2023-08-22 11:47:04 +0300

    content toggle block pattern fix

--- dist/blocks.build.asset.php
+++ dist/blocks.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '29ff637e0385c03bb4af');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => '20ea6cdcb0f49d137359');
