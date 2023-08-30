91322e9b187c5b9deddf7b0c1af7574932f0dbcf
Erdem Bircan <erdem.erdembircan@gmail.com>
2023-08-30 13:37:32 +0300

    phpunit testing base

---
+++ __tests__/php/unit/PHP_Unit_Check_Test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ultimate_Blocks;
+
+use PHPUnit\Framework\TestCase;
+
+class PHP_Unit_Check_Test extends TestCase {
+
+	/**
+	 * Simple test to see phpunit is working
+	 * @return void
+	 */
+	public function testWorking() {
+		$this->assertTrue( true );
+	}
+}
