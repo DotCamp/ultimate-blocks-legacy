84e0d73c30b0dbdb428ebb103b3a4df87bb1c275
erdembircan <erdembircan@users.noreply.github.com>
2023-10-26 12:27:24 +0000

    development build

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'ultimate-blocks/ultimate-blocks',
         'pretty_version' => 'dev-v3_0_6',
         'version' => 'dev-v3_0_6',
-        'reference' => '87d534184bacd5ce526d61833a770a310a18fa7b',
+        'reference' => 'd5f643ff44ea6fdc0785da4bff45187f72fd5afc',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -58,7 +58,7 @@
         'ultimate-blocks/ultimate-blocks' => array(
             'pretty_version' => 'dev-v3_0_6',
             'version' => 'dev-v3_0_6',
-            'reference' => '87d534184bacd5ce526d61833a770a310a18fa7b',
+            'reference' => 'd5f643ff44ea6fdc0785da4bff45187f72fd5afc',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
