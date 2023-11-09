80d6a22d6f9d41aa5b0b2e22149f38e3449495f4
farhan-shafi <farhanshafi34@gmail.com>
2023-11-09 14:10:33 +0500

    fix svg icon attribute console error

--- src/blocks/content-toggle/icons/icon.js
+++ src/blocks/content-toggle/icons/icon.js
@@ -10,14 +10,14 @@ const icon = (
 		<rect x="4" y="2" width="16" height="4" rx="1" fill="#E11B4C" />
 		<rect x="4" y="18" width="16" height="4" rx="1" fill="#E11B4C" />
 		<path
-			fill-rule="evenodd"
-			clip-rule="evenodd"
+			fillRule="evenodd"
+			clipRule="evenodd"
 			d="M4 9C4 8.44772 4.44772 8 5 8H19C19.5523 8 20 8.44772 20 9V15C20 15.5523 19.5523 16 19 16H5C4.44772 16 4 15.5523 4 15V9ZM5 11H19V14C19 14.5523 18.5523 15 18 15H6C5.44772 15 5 14.5523 5 14V11ZM17.2929 9.29289L17 9H17.5858H18.4142H19L18.7071 9.29289L18 10L17.2929 9.29289Z"
 			fill="#E11B4C"
 		/>
 		<path
-			fill-rule="evenodd"
-			clip-rule="evenodd"
+			fillRule="evenodd"
+			clipRule="evenodd"
 			d="M6.5 13C6.5 12.7239 6.72386 12.5 7 12.5H17C17.2761 12.5 17.5 12.7239 17.5 13C17.5 13.2761 17.2761 13.5 17 13.5H7C6.72386 13.5 6.5 13.2761 6.5 13Z"
 			fill="#E11B4C"
 		/>
--- src/blocks/divider/icons/icon.js
+++ src/blocks/divider/icons/icon.js
@@ -8,8 +8,8 @@ const icon = (
 	>
 		<rect width="24" height="24" fill="white" />
 		<path
-			fill-rule="evenodd"
-			clip-rule="evenodd"
+			fillRule="evenodd"
+			clipRule="evenodd"
 			d="M5 3C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3H5ZM6 11C5.44772 11 5 11.4477 5 12C5 12.5523 5.44772 13 6 13H18C18.5523 13 19 12.5523 19 12C19 11.4477 18.5523 11 18 11H6Z"
 			fill="#E11B4C"
 		/>
