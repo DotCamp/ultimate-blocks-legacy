ab2169a8fca59742349fec8b6b7ee995b9928409
Erdem Bircan <erdem.erdembircan@gmail.com>
2024-03-29 16:33:09 +0300

    frontend render tag strip for advanced heading block content

--- src/blocks/advanced-heading/block.php
+++ src/blocks/advanced-heading/block.php
@@ -1,29 +1,35 @@
 <?php
 
-function ub_render_advanced_heading_block($attributes){
-    extract($attributes);
-    $classes = array('ub_advanced_heading');
-    $ids = array();
-    $ids[] = 'ub-advanced-heading-'. $blockID;
-    $block_wrapper_attributes = get_block_wrapper_attributes(
+function ub_render_advanced_heading_block( $attributes ) {
+	extract( $attributes );
+	$classes                  = array( 'ub_advanced_heading' );
+	$ids                      = array();
+	$ids[]                    = 'ub-advanced-heading-' . $blockID;
+	$block_wrapper_attributes = get_block_wrapper_attributes(
 		array(
-			'class' => implode(' ', $classes),
-			'id'    => implode(' ', $ids)
+			'class' => implode( ' ', $classes ),
+			'id'    => implode( ' ', $ids ),
 		)
-    );
-	return '<' . $level . ' ' . $block_wrapper_attributes . ' data-blockid="' . $blockID . '">' . $content . '</' . $level . '>';
+	);
+
+	// Don't allow img and script tags for heading content.
+	$cleaned_content = preg_replace( '/<img[^>]+>/i', '', $content );
+	$cleaned_content = preg_replace( '/<script[^>]*?>.*?<\/script>/is', '', $cleaned_content );
+
+	return '<' . $level . ' ' . $block_wrapper_attributes . ' data-blockid="' . $blockID . '">' . $cleaned_content . '</' . $level . '>';
 }
 
 function ub_register_advanced_heading_block() {
 	if ( function_exists( 'register_block_type_from_metadata' ) ) {
-        require dirname(dirname(__DIR__)) . '/defaults.php';
-		register_block_type_from_metadata( 
-            dirname(dirname(dirname(__DIR__))) . '/dist/blocks/advanced-heading', 
-            array(
-			'attributes' => $defaultValues['ub/advanced-heading']['attributes'],
-			'render_callback' => 'ub_render_advanced_heading_block')
+		require dirname( dirname( __DIR__ ) ) . '/defaults.php';
+		register_block_type_from_metadata(
+			dirname( dirname( dirname( __DIR__ ) ) ) . '/dist/blocks/advanced-heading',
+			array(
+				'attributes'      => $defaultValues['ub/advanced-heading']['attributes'],
+				'render_callback' => 'ub_render_advanced_heading_block',
+			)
 		);
 	}
 }
 
-add_action('init', 'ub_register_advanced_heading_block');
+add_action( 'init', 'ub_register_advanced_heading_block' );
