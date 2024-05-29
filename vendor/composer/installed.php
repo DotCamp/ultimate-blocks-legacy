542f3326d700433a314ac749e3af04c2b96bfa8c
permafrost06 <permafrost06@users.noreply.github.com>
2024-05-29 18:48:29 +0000

    development build

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'ultimate-blocks/ultimate-blocks',
         'pretty_version' => 'dev-v3_1_9',
         'version' => 'dev-v3_1_9',
-        'reference' => '8cb99b6708740ea046ebdfdebf6c75288d598c86',
+        'reference' => 'd668dca7c3bfadbfbbf840902b25b26781b77d2b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -58,7 +58,7 @@
         'ultimate-blocks/ultimate-blocks' => array(
             'pretty_version' => 'dev-v3_1_9',
             'version' => 'dev-v3_1_9',
-            'reference' => '8cb99b6708740ea046ebdfdebf6c75288d598c86',
+            'reference' => 'd668dca7c3bfadbfbbf840902b25b26781b77d2b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
