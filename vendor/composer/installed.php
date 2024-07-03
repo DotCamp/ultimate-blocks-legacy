78b279c7e4cbe5d5b20a4a94696cd41572274727
farhan-shafi <farhan-shafi@users.noreply.github.com>
2024-07-03 07:49:40 +0000

    development build

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'ultimate-blocks/ultimate-blocks',
         'pretty_version' => 'dev-v3_2_1',
         'version' => 'dev-v3_2_1',
-        'reference' => '2696ca811e495523297d593a536f335db6dff998',
+        'reference' => 'a9c6c772ef16b1a40be3aff81e1734efe450b474',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -58,7 +58,7 @@
         'ultimate-blocks/ultimate-blocks' => array(
             'pretty_version' => 'dev-v3_2_1',
             'version' => 'dev-v3_2_1',
-            'reference' => '2696ca811e495523297d593a536f335db6dff998',
+            'reference' => 'a9c6c772ef16b1a40be3aff81e1734efe450b474',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
