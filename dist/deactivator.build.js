d2274f28e7f544ea645add63d718b34d4851b93a
Zahin Azmayeen <zahin06@gmail.com>
2024-08-15 17:05:47 +0600

    remove build artifacts from git

--- dist/deactivator.build.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-if (window.ultimate_blocks) {
-	window.ultimate_blocks.forEach((block) => {
-		if (!block.active) {
-			wp.blocks.unregisterBlockType(block.name);
-		}
-	});
-}
